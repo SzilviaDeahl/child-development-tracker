c3405f1d3e97b8c2c2a06ff07682630268cb02bb
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-10-09 16:39:52 -0600

    angular and express apps separated

--- child-development-tracker/app.js
+++ child-development-tracker/app.js
@@ -5,10 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-// var tracker = require('./routes/tracker');
-
 var api = require('./routes/api');
 
 var app = express();
@@ -25,10 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
-// app.use('/users', users);
 
 app.use('/api', api);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- child-development-tracker/routes/api.js
+++ child-development-tracker/routes/api.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var db = require('monk')('localhost/myTracker');
-var Profile = db.get('profiles')
+var Profile = db.get('profiles');
 
 router.get('/profiles', function (req, res, next) {
-  // Profile.find({}).then(function (err, profiles) {
+  console.log('here in routes');
+  Profile.find({}).then(function(profiles) {
     console.log('In the Profiles Route');
     res.json("you made it");
-})
+ });
+});
 
 router.post('/profiles', function (req, res, next) {
-  // Insert new profile into database
-  // then respond with json
-  res.json("Profile created")
-})
+  console.log("IN THE POST ROUTE");
+  res.json("Here")
+  Profile.create(
+  {
+    first: req.body.first,
+    middle: req.body.middle,
+    last: req.body.last,
+    dob: req.body.dob
+
+  }).then(function (response) {
+    res.json("Profile created")
+  })
+});
 
 module.exports = router;
